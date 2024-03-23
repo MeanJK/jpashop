178a34e0cd3c13452319a53af6359e0b8d784c8d
MJ <rlaalswls102@naver.com>
2024-03-23 23:07:17 +0900

    BookForm.java 업데이트

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -10,8 +10,11 @@ public class BookForm {
     private Long id;
 
     private String name;
+
     private int price;
+
     private int stockQuantity;
+
     private String author;
 
     private String isbn;
