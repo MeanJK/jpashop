f7bc70a3f5d954afb198194776ff95b8a86e235a
MJ <rlaalswls102@naver.com>
2024-03-27 01:59:18 +0900

    feat: 03/26 스프링 JPA 활용 2 1+N 번의 쿼리가 발생할 때 성능 최적화하는 방법의 단계와 fetch join의 사용법★

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,9 +1,19 @@
 package jpabook.jpashop.api;
 
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 * X To One (Many To One, One To One)
@@ -17,4 +27,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all){
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getMember().getAddress(); // Lazy 강제 초기화 해서 문제가 있다.
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){ // 원래는 List가 아닌 result로 감싸야함.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화 -> memberId를 갖고 영속성 컨텍스트를 찾아본 후 없다면 db에 쿼리를 날림
+            orderDate = order.getOrderDate();    // 즉-> 많은 쿼리를 날리므로 시간이 오래걸린다 효율 -
+            orderStatus = order.getOrderStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
